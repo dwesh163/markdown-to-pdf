90153dec5aabbbc1426bcc061d4ee1766b3180ab
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-11-19 11:25:45 +0100

    refactor MarkdownPreview component to replace ReactMarkdown with markdown-to-jsx for improved rendering

--- src/components/markdown-preview.tsx
+++ src/components/markdown-preview.tsx
@@ -1,10 +1,6 @@
 'use client';
 
-import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { tomorrow } from 'react-syntax-highlighter/dist/esm/styles/prism';
-
+import Markdown from 'markdown-to-jsx';
 interface MarkdownPreviewProps {
 	markdown: string;
 }
@@ -12,25 +8,7 @@ interface MarkdownPreviewProps {
 export function MarkdownPreview({ markdown }: MarkdownPreviewProps) {
 	return (
 		<div className="preview-content prose dark:prose-invert max-w-none h-[600px] overflow-y-auto p-6 border rounded-lg bg-gray-50 dark:bg-gray-800">
-			<ReactMarkdown
-				remarkPlugins={[remarkGfm]}
-				components={{
-					code({ node, className, children, ...props }) {
-						const match = /language-(\w+)/.exec(className || '');
-						return match ? (
-							// @ts-ignore
-							<SyntaxHighlighter style={tomorrow} language={match[1]} PreTag="div" {...props}>
-								{String(children).replace(/\n$/, '')}
-							</SyntaxHighlighter>
-						) : (
-							<code className={className || ''} {...props}>
-								{children}
-							</code>
-						);
-					},
-				}}>
-				{markdown || '*No content to display*'}
-			</ReactMarkdown>
+			<Markdown>{markdown || '*No content to display*'}</Markdown>
 		</div>
 	);
 }
